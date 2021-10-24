485c3d1f855eaa39f0bb7938fb19f7e01dddb053
Sounak Ghosh <58182994+sounakivan@users.noreply.github.com>
2021-10-24 13:18:21 -0400

    Basic set up and rotating rings

---
+++ Assets/Scripts/RotatingRing.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingRing : MonoBehaviour
+{
+    public float rotationSpeed = 10;
+    private Vector3 rotAxis;
+    public bool clockwise = true;
+    private int direction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (clockwise)
+        {
+            direction = 1;
+        }
+        else
+        {
+            direction = -1;
+        }
+        rotAxis.Set(0, direction, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(rotAxis * rotationSpeed * Time.deltaTime);
+
+        if (Input.GetKey("d"))
+        {
+            direction = -direction;
+            
+            rotAxis.Set(0, direction, 0);
+        }
+    }
+
+}
